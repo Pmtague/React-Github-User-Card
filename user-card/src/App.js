55b782a5f83d39532acc45ac62feb4881c5d527e
Penny Tague <pmtague9@gmail.com>
2019-08-06 15:44:58 -0700

    Got user info to show, working on displaying followers

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,26 +1,78 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import UserCard from './components/UserCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+      userInfo: [],
+      followers: []
+    }
+
+  componentDidMount() {
+    console.log('CDM invoked in App');
+
+    fetch('https://api.github.com/users/Pmtague')
+      .then(res => {
+        return res.json();
+      })
+
+      .then(info => this.setState({ userInfo: info}))
+
+      .catch(err => {
+        console.log('Oh no, an error!', err)
+      })
+    
+    fetch('https://api.github.com/users/Pmtague/followers')
+      .then(res => {
+        return res.json();
+      })
+
+      .then(data => this.setState({ followers: data }))
+
+      .catch(err => {
+        console.log('Oopsie whoopsie!', err)
+      })
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('CDU Running!');
+    console.log('Prev Props!', prevProps);
+    console.log('Prev State!', prevState);
+
+    if (this.props.userID !== prevProps.userID) {
+      console.log('Never will run!');
+      this.fetchData(this.props.userID)
+    }
+
+    if(prevState !== this.state.userInfo) {
+      console.log('Change in state!')
+    }
+
+    if (this.props.userID !== prevProps.userID) {
+      console.log('Never will run!');
+      this.fetchData(this.props.userID)
+    }
+
+    if(prevState !== this.state.followers) {
+      console.log('Change in follower state!')
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className='App-header'>
+          <h2>Penny's GitHub User Card</h2>
+        </header>
+        <UserCard 
+            name={ this.state.userInfo.name }
+            repos={ this.state.userInfo.public_repos }
+            following={ this.state.userInfo.following }
+            followers={ this.state.followers }
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ user-card/src/components/UserCard.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const UserCard = props => {
+    console.log('PropsPropsProps', props)
+    const Wrapper = styled.div`
+        vh: 90%;
+        vw: 70%;
+        `;
+
+    const Image = styled.img`
+        height: 90%;
+        width: auto;
+        `;
+    
+    return (
+        <Wrapper>
+            <Image />
+            <h3>{ props.name }</h3>
+            <p>Public Repos: { props.repos }</p> 
+            <p>Following: { props.following }</p>
+            <p>Followers: { props.followers.map(item => (
+                <div key={ item } className='followers-list'>
+                    { item }
+                </div>
+            )) }</p>
+        </Wrapper>
+    )
+}
+
+export default UserCard;
